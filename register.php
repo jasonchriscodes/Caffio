869e6e8e31558d9e68bd358715458b2b7b328baf
Jason Christian <jasonchristian1234@gmail.com>
2026-06-25 13:31:45 +1200

    chore: add register.php

---
+++ register.php
@@ -0,0 +1,107 @@
+<?php
+    include './components/connect.php';
+    session_start();
+
+    if(isset($_SESSION['user_id'])){
+    $user_id = $_SESSION['user_id'];
+    } else {
+        $user_id = '';
+    }
+
+    // Register user
+    
+    if(isset($_POST['submit'])){
+        $id = unique_id();
+        $name = $_POST['name'];
+        $name = filter_var($name, FILTER_SANITIZE_STRING);
+        $email = $_POST['email'];
+        $email = filter_var($email, FILTER_SANITIZE_STRING);
+        $pass = $_POST['pass'];
+        $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+        $cpass = $_POST['cpass'];
+        $cpass = filter_var($cpass, FILTER_SANITIZE_STRING);
+
+        $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ?");
+        $select_user->execute([$email]);
+        $row = $select_user->fetch(PDO::FETCH_ASSOC);
+
+        if($select_user->rowCount() > 0) {
+            $warning_msg[] = 'email already exists';
+            echo 'email already exists';
+        } else {
+            if ($pass != $cpass) {
+                $warning_msg[] = 'confirm your password';
+                echo 'confirm your password';
+            } else {
+                $insert_user = $conn->prepare("INSERT INTO `users`(user_id, name, email, password) VALUES(?,?,?,?)");
+                $insert_user->execute([$id, $name, $email, $pass]);
+                $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? AND password = ?");
+                $select_user->execute([$email, $pass]);
+                $row = $select_user->fetch(PDO::FETCH_ASSOC);
+                if ($select_user->rowCount() > 0) {
+                    $_SESSION['user_id'] = $row['user_id'];
+                    $_SESSION['user_name'] = $row['name'];
+                    $_SESSION['user_email'] = $row['email'];
+                    header('location: login.php');
+                } 
+            }
+        }
+    }
+?>
+
+<style type="text/css">
+<?php include './style.css';
+?>
+</style>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Caffio</title>
+    <link rel="stylesheet" type="text/css" href="./style.css">
+</head>
+
+<body>
+    <div class="main-container">
+        <section class="form-container">
+            <div class="title">
+                <img src="./img/download.png" alt="">
+                <h1>Register Now</h1>
+                <p>Your Journey to Wellness Starts Here - Register Now!</p>
+            </div>
+            <form accept="" method="post">
+                <div class="input-field">
+                    <p>Your name <sub></sub></p>
+                    <input type="text" name="name" required placeholder="Enter your name" maxlength="50">
+                </div>
+
+                <div class="input-field">
+                    <p>Your Email <sub>*</sub></p>
+                    <input type="email" name="email" required placeholder="Enter your email" maxlength="50"
+                        oninput="this.value = this.value.replace(/\s/g, '')">
+
+                </div>
+                <div class="input-field">
+                    <p>Your Password <sub>*</sub></p>
+                    <input type="password" name="password" required placeholder="Enter your password" maxlength="50"
+                        oninput="this.value = this.value.replace(/\s/g, '')">
+                </div>
+
+                <div class="input-field">
+                    <p>Confirm Password <sub>*</sub></p>
+                    <input type="password" name="cpass" required placeholder="Enter your password" maxlength="50"
+                        oninput="this.value = this.value.replace(/\s/g, '')">
+                </div>
+                <input type="submit" name="submit" value="register now" class="btn">
+                <p>already have an account? <a href="login.php">Login Now</a></p>
+            </form>
+        </section>
+    </div>
+
+    <?php include './components/alert.php'; ?>
+</body>
+
+</html>
